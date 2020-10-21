403bcfcec596ad6c795900c5a2f13031a6e4e9fb
rijkin <reijqueenox@yahoo.com>
2020-10-21 19:08:26 +0300

    update 1.2

---
+++ console/migrations/m201021_160022_create_scovealca_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%scovealca}}`.
+ */
+class m201021_160022_create_scovealca_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%scovealca}}', [
+            'name' => $this->string(),
+            'email' => $this->string(),
+            'phone' => $this->int(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%scovealca}}');
+    }
+}
